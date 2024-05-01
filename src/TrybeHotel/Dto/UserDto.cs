1872a36a54a47d7150778aa534eb0046d52170c9
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-05-01 11:44:35 -0300

    2.. Desenvolva o endpoint POST /user/DONE

--- src/TrybeHotel/Dto/UserDto.cs
+++ src/TrybeHotel/Dto/UserDto.cs
@@ -1,11 +1,16 @@
 namespace TrybeHotel.Dto
 {
     public class UserDto {
-
+        public int userId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? userType { get; set; }
     }
 
     public class UserDtoInsert {
-       
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
 
     public class LoginDto {
